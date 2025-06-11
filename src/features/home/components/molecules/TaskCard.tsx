6c2cfb01d4ecc8615b557f3247b30bc3663cb7fb
Ange <monsecahuum@gmail.com>
2025-06-11 16:29:56 -0600

    Estructura basica de las actividades diarias programadas (datos provisionales)

---
+++ src/features/home/components/molecules/TaskCard.tsx
@@ -0,0 +1,75 @@
+import { View, Text, StyleSheet } from "react-native";
+
+/*Se declaran los tipos de la tarjeta de tareas */
+type TaskCardProps = {
+    time: string;
+    title: string;
+    /*Se tiene como opciones: */
+    description?: string;
+    color?: string;
+};
+
+/*Se crea la funcion de la tajeta que resivira 4 props anteriormente declaras*/
+export default function TaskCard({time, title, description, color='#6096B4'}: TaskCardProps){
+ return (
+   <View style={styles.container}>
+    {/*Se muestra la hora de la tarea */}
+        <Text style={styles.time}>{time}</Text>
+        {/*Tarjeta de tarea*/}
+        <View style={styles.card}>
+            {/*Franja de colores */}
+            <View style={[styles.colorBar, { backgroundColor: color}]}/>
+            {/*Información del taskcard */}
+            <View style={styles.content}>
+                {/*Se visualiza el titulo */}
+                <Text style={styles.title}>{title}</Text>
+                {/*Muestra la descripción en caso de que exista */}
+                {description && <Text style={styles.description}>{description}</Text>}
+            </View>
+        </View>
+   </View> 
+ )
+}
+
+{/*Se crean los estilos correspondientes*/}
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 20,
+        marginBottom: 10,
+        paddingHorizontal: 16,
+    },
+    time: {
+        fontSize: 12,
+        color: '#888',
+        marginBottom: 4,
+    },
+    card: {
+        flexDirection: 'row',
+        backgroundColor: '#fff',
+        borderRadius: 8,
+        elevation: 2,
+        shadowColor: '#000',
+        shadowOpacity: 0.1,
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 4,
+        overflow: 'hidden',
+    },
+    colorBar: {
+        width: 9,
+    },
+    content: {
+        padding: 12,
+        flex: 1,
+    },
+    title: {
+        fontSize: 16,
+        fontWeight: '600',
+        color: '#333',
+    },
+    description: {
+        fontSize: 14,
+        color: '#666',
+        marginTop: 4,
+    },
+});
+
--- src/features/home/screens/HomeScreen.tsx
+++ src/features/home/screens/HomeScreen.tsx
@@ -1,15 +1,28 @@
 import { View, StyleSheet} from "react-native";
 import HomeHeader from "../components/organisms/HomeHeader";
 import DaySelector from "../components/molecules/DaySelector";
+import TaskCard from './../components/molecules/TaskCard';
 
 
 export default function HomeScreen(){
-    return (
-        <View style={styles.container}>
-            <HomeHeader />
-            <DaySelector />
-        </View>
-    )
+  return (
+    <View style={styles.container}>
+      <HomeHeader />
+      <DaySelector />
+      {/*Se prueba el diseño de la TaskCard con datos provisionales*/}
+      <TaskCard
+        time="09:00 AM"
+        title="Reunión con Ana"
+        description="Revisar entregables semanales"
+        color="#93BFCF" />
+      <TaskCard
+        time="11:30 AM"
+        title="Llamada con Pedro"
+        description="Confirmar detalles del CRM"
+        color="#BDCDD6"
+      />
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
